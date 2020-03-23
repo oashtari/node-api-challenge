df3e588a506b1dd228ad3bfe76307a0a23a622a5
oashtari <oashtari@gmail.com>
2020-03-22 19:59:59 -0700

    added post for actions

--- data/helpers/actionsRouter.js
+++ data/helpers/actionsRouter.js
@@ -26,11 +26,37 @@ router.get('/', (req, res) => {
 
 //POST
 
+router.post('/', validateAction, (req, res) => {
+    const action = req.body;
+    console.log('what is req body', req.body);
+
+    db.insert(action)
+        .then(action => { res.status(201).json(action) })
+        .catch(error => {
+            res.status(500).json({ message: "could not add action to database" })
+        })
+});
 
 //PUT
 
 
 //DELETE
+
+
+//MIDDLEWARE
+
+function validateAction(req, res, next) {
+    if (req.body) {
+        if (req.body.project_id && req.body.description && req.body.notes) {
+            next()
+        } else {
+            res.status(400).json({ message: "missing required id, description and/or notes fields" })
+        }
+    } else {
+        res.status(400).json({ message: "missing user data" })
+    }
+}
+
 
 
 module.exports = router;
