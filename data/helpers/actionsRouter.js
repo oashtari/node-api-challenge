c270ccda18ba1aa3fb5697fd1e00386cc1fc2b50
oashtari <oashtari@gmail.com>
2020-03-22 20:20:29 -0700

    finished put for actions

--- data/helpers/actionsRouter.js
+++ data/helpers/actionsRouter.js
@@ -24,11 +24,11 @@ router.get('/', (req, res) => {
 })
 
 
+
 //POST
 
 router.post('/', validateAction, (req, res) => {
     const action = req.body;
-    console.log('what is req body', req.body);
 
     db.insert(action)
         .then(action => { res.status(201).json(action) })
@@ -38,6 +38,25 @@ router.post('/', validateAction, (req, res) => {
 });
 
 //PUT
+
+router.put('/:id', validateAction, (req, res) => {
+    const { id } = req.params;
+    const action = req.body;
+    console.log('here is the req body', req.body)
+
+    db.update(id, action)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json(updated)
+            } else {
+                res.status(404).json({ message: "that action could not be found" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "could not get that project" })
+        })
+});
+
 
 
 //DELETE
@@ -53,10 +72,21 @@ function validateAction(req, res, next) {
             res.status(400).json({ message: "missing required id, description and/or notes fields" })
         }
     } else {
-        res.status(400).json({ message: "missing user data" })
+        res.status(400).json({ message: "missing action data" })
     }
 }
 
+function validatePutAction(req, res, next) {
+    if (req.body) {
+        if (req.body.description && req.body.notes) {
+            next()
+        } else {
+            res.status(400).json({ message: "missing required description and/or notes fields" })
+        }
+    } else {
+        res.status(400).json({ message: "missing action data" })
+    }
+}
 
 
 module.exports = router;
