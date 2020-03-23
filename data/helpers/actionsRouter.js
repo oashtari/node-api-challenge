e598308af65b7cdb1287c78c792505d611bfd59e
oashtari <oashtari@gmail.com>
2020-03-22 19:45:47 -0700

    finished get for actions

--- data/helpers/actionsRouter.js
+++ data/helpers/actionsRouter.js
@@ -4,7 +4,9 @@ const db = require('./actionModel');
 
 const router = express.Router();
 
-
+// insert,
+// update,
+// remove,
 
 //GET
 
@@ -13,7 +15,7 @@ router.get('/', (req, res) => {
     db.get()
         .then(actions => {
             if (actions) {
-                res.status(200).json(projects)
+                res.status(200).json(actions)
             } else {
                 res.status(404).json({ message: 'could not get your projects' })
             }
@@ -21,10 +23,6 @@ router.get('/', (req, res) => {
         .catch(error => res.status(500).json({ error: "unable to hit actions database" }))
 })
 
-
-// server.get("/", (req, res) => {
-//     actionDb.get().then(actions => res.status(200).json(actions));
-// });
 
 //POST
 
